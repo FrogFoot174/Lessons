d49bd543d74eb5d2425cc5a734ed7de1e007d885
FrogFoot174 <fogot6@yandex.ru>
2015-02-17 16:54:25 +0500

    Добавил связного sql

---
+++ dz1/www/functions/sql.php
@@ -0,0 +1,12 @@
+<?php
+
+function Sql_connect()
+{
+    mysql_connect('localhost', 'root', '');
+    mysql_select_db('test');
+}
+
+function Sql_query()
+{
+
+}
--- dz1/www/models/news.php
+++ dz1/www/models/news.php
@@ -1,13 +1,18 @@
 <?php
+require_once __DIR__ . '/../functions/sql.php';
 function News_getAll ()
 {
-mysql_connect('localhost', 'root', '');
-mysql_select_db('test');
-$sql = 'SELECT * FROM articles';
-$res = mysql_query($sql);
-$ret = [];
-while (false!== $row = mysql_fetch_assoc($res)){
+    Sql_connect();
+    $sql = 'SELECT * FROM articles';
+    $res = mysql_query($sql);
+    $ret = [];
+    while (false!== $row = mysql_fetch_assoc($res)){
     $ret[] = $row;
 }
     return $ret;
+}
+
+function News_insert($data)
+{
+
 }
