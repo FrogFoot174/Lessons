a2d2aa6c04f3a15a1cddeb5125f77a92146060b7
FrogFoot174 <fogot6@yandex.ru>
2015-02-19 00:19:54 +0500

    Complete

--- dz1/www/views/index.php
+++ dz1/www/views/index.php
@@ -6,8 +6,8 @@
 </head>
 <body>
         <?php foreach ($articles as $article): ?>
-    <div style="border: solid; border-color:  deepskyblue; width: 400px; margin-bottom: 25px">
-        <?php echo $article['title']; ?>
+    <div style="border: solid; border-color:  deepskyblue; width: 1025px; margin-bottom: 25px">
+        <img src="<?php echo '.' . $article['path']; ?>" alt=""/>
     </div>
         <?php endforeach ?>
         <a href="./add.php">Добавьте новость сами</a>
